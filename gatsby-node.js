a554a31e610b2300519a7212a55e32a4aaed61c8
Daniel Kolev <daniel.kolev@isobar.com>
2021-07-01 06:57:17 +0300

    Update api key and handle undefined error

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ exports.sourceNodes = async ({
   actions: { createNode },
   createContentDigest,
 }) => {
-  const token = 'IGQVJWOUstZAjhtNXJUU2RoLXZApSmZAGNmx5VGVKMzU1WGpNT2V2QXhrcEI4ZAW5NS01mR25tUDRiRGtLb3psZAmt4YW1SUUR0czdEeWt5UkxJM3FjeVVhekdtdjhlMFRnX0RxSGhKb3h1SG45TmgyLWM0YgZDZD'
+  const token = 'IGQVJWdi1EVmM3UnlXVTM3bG4zc1YyOUU0bDV5eXg4TmNoZAHplc1ZAobm9tQUVKZAElGeEM0TlFFTndSWWVOVmN5ZAGllR2o0cFFqUzBGRkpna0F3VUVtUXMyYzBCQVRxMjdiRWRpcHFGR0VYdkFjbk1GMgZDZD'
   let images = []
   await fetch('https://graph.instagram.com/me/media?access_token=' + token + '&fields=media_url,permalink,caption,media_type,children{id,media_url,permalink}', {
       method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -15,8 +15,8 @@ exports.sourceNodes = async ({
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
   })
       .then(response => response.json())
-      .then(data => {
-        images = data.data
+      .then(({data = []}) => {
+        images = data
         .filter((image) => image.media_type !== 'VIDEO')
         .map((image) => { 
           return {
